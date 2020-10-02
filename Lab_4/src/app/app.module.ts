0b9d11838dcd9e88408e43f84f9d588412409f14
pnakajitti <pnakajitti@gmail.com>
2020-10-02 09:27:35 -0400

    Lab_4

---
+++ Lab_4/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import {Routes} from '@angular/router';
+import {routing} from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent
+  ],
+  imports: [
+    routing,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
